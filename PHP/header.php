039841a2b6d7c7479ecbb89431f0ccf924abbe2b
WeirdlyWired <r.pastori1@campus.unimib.it>
2021-03-02 14:55:42 +0100

    update header.php

--- PHP/header.php
+++ PHP/header.php
@@ -1,11 +1,13 @@
 /* Estratto del file header.php contenente le modifiche per l'aggiunta del file di stile personalizzato e dell'Iconfont */
 
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="preload" href="/wp-content/themes/generatepress/assets/css/new.css" as="style">
+    <link rel="preload" href="/wp-content/themes/generatepress/assets/css/addtional.css" as="style">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-    <link rel="stylesheet" href="/wp-content/themes/generatepress/assets/css/plus.css" type="text/css">
+    <link rel="stylesheet" href="/wp-content/themes/generatepress/assets/css/additional.css" type="text/css">
 	<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9a56da31a8b16a0678cbc68ca/bdc3908e76efcb30bd908fe3c.js");</script>
     <?php wp_head(); ?>
 </head>
