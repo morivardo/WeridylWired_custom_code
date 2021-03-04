023bf346c5ef001a3596ddc7c0cd25e40068324a
WeirdlyWired <r.pastori1@campus.unimib.it>
2021-03-04 11:03:04 +0100

    fix presentation-link

--- PHP/header.php
+++ PHP/header.php
@@ -5,9 +5,9 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="preload" href="/wp-content/themes/generatepress/assets/css/restyle.css" as="style">
+    <link rel="preload" href="/wp-content/themes/generatepress/assets/css/restyles.css" as="style">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" integrity="sha384-vp86vTRFVJgpjF9jiIGPEEqYqlDwgyBgEF109VFjmqGmIY/Y4HV4d3Gp2irVfcrp" crossorigin="anonymous">
-    <link rel="stylesheet" href="/wp-content/themes/generatepress/assets/css/restyle.css" type="text/css">
+    <link rel="stylesheet" href="/wp-content/themes/generatepress/assets/css/restyles.css" type="text/css">
 	<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/9a56da31a8b16a0678cbc68ca/bdc3908e76efcb30bd908fe3c.js");</script>
     <?php wp_head(); ?>
 </head>
